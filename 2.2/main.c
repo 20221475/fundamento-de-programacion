de5a9f8a80bb0cfecdce92a0ec19f9c83243b0e3
Ruddy Gonzalez <105997424+20221475@users.noreply.github.com>
2022-11-04 00:26:18 -0400

    Add files via upload

---
+++ 2.2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* promedio de curso
+si el promedio es igual o mayor que 6 aprobado
+
+pro: variable de tipo real. */
+int main(void) {
+  float pro;
+  printf("ingresa el promedio del alumno\n");
+  scanf("%f", &pro);
+    if (pro >=6);
+  printf("\naprobado");
+  return 0;
+}
+
