7c48c1cfbc359714deb065dce1ee9ef6ab744b43
Ruddy Gonzalez <105997424+20221475@users.noreply.github.com>
2022-11-04 11:53:24 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
-#include <stdio.h>
+/* promedio de curso
+si el promedio es igual o mayor que 6 aprobado
 
-/* incremento de salario.
-el programa al recibir como dato el nivel de un profesor, incrementa su
-salario en funcion de la tabla 2.3.
-
-NIV: variable de tipo entero.
-SAL: variable de tipo real. */
-
-void main(void)
-{
-    float SAL;
-    int NIV;
-    printf("Ingrese el nivel academico del profesor: ");
-    scanf("%d", &NIV);
-
-    printf("Ingrese el salario: ");
-    scanf("%f", &SAL);
-    switch(NIV);
-    printf("Ingrese el salario: ");
-    scanf("%f", &SAL);
-    switch(NIV);
-   {
-
-        case 1: SAL = SAL * 1.0035; break;
-        case 2: SAL = SAL * 1.0041; break;
-        case 3: SAL = SAL * 1.0048; break;
-        case 4: SAL = SAL * 1.0053; break;
-   }
-
- printf("/n/nNivel: %d /tNuevo salario: %8,2f" ,NIV, SAL);
-
- return 0;
+pro: variable de tipo real. */
+int main(void) {
+  float pro;
+  printf("ingresa el promedio del alumno\n");
+  scanf("%f", &pro);
+    if (pro >=6);
+  printf("\naprobado");
+  return 0;
 }
+
