a1b89ac7ce45b7aa7f052729625ba8a7a4c4231e
Ruddy Gonzalez <105997424+20221475@users.noreply.github.com>
2022-11-11 00:25:36 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
-/* promedio de curso
-si el promedio es igual o mayor que 6 aprobado
 
-pro: variable de tipo real. */
-int main(void) {
-  float pro;
-  printf("ingresa el promedio del alumno\n");
-  scanf("%f", &pro);
-    if (pro >=6);
-  printf("\naprobado");
-  return 0;
-}
+/*cubo-2.
+el programa calcula el cubo de los 10 primero numero naturales con la ayuda de una funcion. */
 
+ int cubo(void);      /*prototipo de funcion*/
+ int I;               /*variable global*/
+
+ void main(void)
+ {
+     int CUB;
+     for (I = 1; I <= 10; I++)
+     {
+         CUB = cubo();   /*llamada ala funcion cubo. */
+         printf("\nEl cubo de %d es:  %d", I, CUB);
+     }
+ }
+
+ int cubo(void)    /*declaracion de la funcion*/
+ /*la funcion calcula el cubo de la variable local I. */
+ {
+     int I = 2;    /* variable local entera I con el mismo nombre que la variable global*/
+     return(I*I*I);
+ }
