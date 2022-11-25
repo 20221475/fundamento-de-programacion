1d292146f419b2fea50099b1872fb605e6f8a142
Ruddy Gonzalez <105997424+20221475@users.noreply.github.com>
2022-11-25 08:26:53 -0400

    Add files via upload

--- Untitled3.c
+++ Untitled3.c
@@ -1,27 +1,88 @@
 #include <stdio.h>
-
-/* funcion y parametro. */
-
- int a, b, c, d;
-                                     /*prototipo de funcion.*/
- int  pal(int, int);
- void main(void)
- {
-  a = 2;
-  c = 3;
-  d = 5;
-  a = pal(c,d);
-  printf("\n%d %d %d %d", a, b, c, d);
-  b = 4;
-  b = pal(b, a);
-  printf("\n%d %d %d %d", a, b, c, d);
-  }
-  int pal(int x, int y);
-  {
-  int c;
-  b = x * y;
-  c = b + y;
-  x++;
-  y = y * (y + 1);
-  printf("\n%d %d %d %d", b, c, x, y);
-  return(x);
+#include <math.h>
+/* Estadístico.
+El programa, al recibir como dato un arreglo unidimensional de enteros
+que contiene calificaciones, calcula la media, la varianza, la
+desviación estándar y la moda. */
+const int MAX = 100;
+void Lectura(int *, int);
+float Media(int *, int);
+float Varianza(int *, int, float); /* Prototipos de funciones. */
+float Desviacion(float);
+void Frecuencia(int *, int, int *);
+int Moda(int *, int);
+void main(void)
+{
+int TAM, MOD, ALU[MAX], FRE[11] = {0};
+float MED, VAR, DES;
+do
+{
+printf("Ingrese el tamaño del arreglo: ");
+scanf("%d", &TAM);
+}
+while (TAM > MAX || TAM < 1);
+/* Se verifica que el tamaño del arreglo sea correcto. */
+Lectura(ALU, TAM);
+MED = Media(ALU, TAM);
+VAR = Varianza(ALU, TAM, MED);
+DES = Desviacion(VAR);
+Frecuencia(ALU, TAM, FRE);
+MOD = Moda(FRE, 11);
+printf("\nMedia: %.2f", MED);
+printf("\nVarianza: %.2f", VAR);
+printf("\nDesviación: %.2f", DES);
+printf("\nModa: %d", MOD);
+}
+void Lectura(int A[], int T)
+/* La función Lectura se utiliza para leer un arreglo unidimensional de T elementos de tipo entero. */
+{
+int I;
+for (I=0; I<T; I++)
+{
+printf("Ingrese el elemento %d: ", I+1);
+scanf("%d", &A[I]);
+}
+}
+float Media (int A[], int T)
+/* Esta función se utiliza para calcular la media. */
+{
+int I;
+float SUM = 0.0;
+for (I=0; I < T; I++)
+SUM += A[I];
+return (SUM / T);
+}
+float Varianza(int A[], int T, float M)
+/* Esta función se utiliza para calcular la varianza. */
+{
+int I;
+float SUM = 0.0;
+for (I=0; I < T; I++)
+SUM += pow ((A[I] - M), 2);
+return (SUM / T);
+}
+float Desviacion(float V)
+/* Esta función se utiliza para calcular la desviación estándar. */
+{
+return (sqrt(V));
+}
+void Frecuencia(int A[], int P, int B[])
+/* Esta función se utiliza para calcular la frecuencia de calificaciones.
+*/
+{
+int I;
+for (I=0; I < P; I++)
+B[A[I]]++;
+}
+int Moda(int A[], int T)
+/* Esta función se utiliza para calcular la moda. */
+{
+int I, MOD = 0, VAL = A[0];
+for (I=1; I<T; I++)
+if (MOD < A[I])
+{
+MOD = I;
+VAL = A[I];
+}
+return (MOD);
+}
